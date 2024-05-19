76df1888810adc2bcf682474ad6717846590686e
sexylad22 <154746508+sexylad22@users.noreply.github.com>
2024-05-19 20:01:14 +0300

    third commit

--- dist/code.js
+++ dist/code.js
@@ -41,5 +41,38 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const text = document.querySelector('.text')
+//     const targetDiv = document.querySelector('.targetdiv');
+//     const targetDivTopOffset = targetDiv.offsetTop;
+//     const icons = document.querySelector('.icons');
+//     const myimg = document.querySelector('.myimg');
+//     const div1 = document.querySelector('.div1');
+
+//     window.addEventListener('scroll', () => {
+//         if (window.pageYOffset >= targetDivTopOffset) {
+//             // Add or remove classes as needed
+//             myimg.src = "ninit.png";
+
+//             div1.classList.remove("bg-white")
+//             div1.classList.add("bg-[#533223]")
+//             text.classList.remove("text-white");
+//             text.classList.add("text-[#533223]");
+//             icons.classList.remove("text-white");
+//             icons.classList.add("text-[#533223]");
+
+//         } else {
+//             // Reset the classes when the fixed div scrolls back up
+//             myimg.src = "white.png";
+//             div1.classList.remove("bg-[#533223]")
+//             div1.classList.add("bg-white")
+//             text.classList.remove("text-[#533223]");
+//             text.classList.add("text-white");
+//             icons.classList.remove("text-[#533223]");
+//             icons.classList.add("text-white");
+            
+//         }
+//     });
+// });
 
 
